a32ace62b30bb42a122a2924b5dfbfcb3646f494
Ito Haruto <s1701015@sendai-nct.jp>
2020-12-10 14:28:29 +0900

    (temporary)Define Field.cs

---
+++ Field.cs
@@ -0,0 +1,44 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SugorokuServer
+{
+	class Field
+	{
+		Square[] course;
+		Player[] players; //4
+		int cource_lenghs;
+		int turm;
+
+	}
+
+
+	class Square
+	{
+		int index;
+		EventType eventType;
+	}
+	
+
+	class Player
+	{
+		string name;
+		int point;
+		int playerId;
+	}
+
+
+	enum EventType
+	{
+
+	}
+
+
+	class PlayerAction
+	{
+		int playerId;
+		int moveLength;
+	}
+
+}
